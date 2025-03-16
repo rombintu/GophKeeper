df9ccb5515b7406889b5cdb4e09dd54c03e36d35
Tigran <aruttigran1203@yandex.ru>
2025-03-16 22:04:28 +0300

    add sync handlers templates

---
+++ internal/sync/handlers.go
@@ -0,0 +1,18 @@
+package sync
+
+// import (
+// 	"context"
+// 	"log/slog"
+
+// 	spb "github.com/rombintu/GophKeeper/internal/proto/sync"
+// 	"github.com/rombintu/GophKeeper/lib/common"
+// 	"google.golang.org/protobuf/types/known/emptypb"
+// )
+
+// func (s *SyncService) SyncPush(ctx context.Context, in *spb.SyncRequest) (*spb.SyncResponse, error) {
+// 	s.store.Get(ctx, )
+// }
+
+// func (s *SyncService) SyncPull(ctx context.Context, in *spb.SyncRequest) (*spb.SyncResponse, error) {
+
+// }
